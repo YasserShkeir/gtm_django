b78e04b744debef612ba6a5d76891190653ea909
Yasser Shkeir <shkeiryasser@gmail.com>
2023-09-21 23:52:43 +0300

    Models; Content and Language Models

--- gtm_django/content/models.py
+++ gtm_django/content/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Language(models.Model):
+    name = models.CharField(max_length=255)
+
+class Content(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    points = models.IntegerField()
+    creationDate = models.DateTimeField(auto_now_add=True)
